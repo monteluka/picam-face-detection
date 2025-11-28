e6e3eb945d560c5158f589c90b304a206451ccc8
Monte <damonter@uci.edu>
2025-11-28 01:46:58 -0800

    Background of cat image is now removed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # number has to be in range [0, 15]
 def getCatHead(catHeadsImg, number):
@@ -10,6 +11,31 @@ def getCatHead(catHeadsImg, number):
     j = number % 4
     smallerImg = catHeadsImg[y[j+0]:y[j+1],x[i+0]:x[i+1],:]
     return smallerImg
+
+# returns a list where pixels that are background are marked as 0 (0x0)
+# and foreground pixels are marked as 255 (0xFF)
+def createMask(catHeadImg):
+    # first convert the image to grayscale for easier processing
+    greyCatHeadImg = cv2.cvtColor(catHeadImg, cv2.COLOR_BGR2GRAY)
+
+    # now set up the thresholds for what colors we consider background
+    _, threshold = cv2.threshold(greyCatHeadImg, 240, 255, cv2.THRESH_BINARY_INV)
+
+    # now detect contours (shapes)
+    # this will be used to find the shape of the cat's face
+    # solves the problem of the white inside cat's face being selected for removal as well
+    contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # the largest contour in the image will be the cat's face
+    catHeadContour = max(contours, key=cv2.contourArea)
+
+    # create a mask with the same height and width as the image
+    mask = np.zeros_like(greyCatHeadImg)
+
+    # fill the elements in the mask that correspond with the foreground area of the image with white pixels (0xFF)
+    cv2.drawContours(mask, [catHeadContour], -1, 255, thickness=cv2.FILLED)
+
+    return mask
 
 def main():
     img = cv2.imread("./res/faces.jpg")
@@ -31,7 +57,13 @@ def main():
         # now place cat head over face
         catHeadImg = getCatHead(catHeadsImg, 0)
         catHeadImg = cv2.resize(catHeadImg, (width, height))
-        img[y:y+height, x:x+width] = catHeadImg
+        # create a mask of the image the separates background and foreground
+        catHeadMask = createMask(catHeadImg)
+        # define the pixels that make up the detected face region
+        faceArea = img[y:y+height, x:x+width]
+        # pixels in the facial area will be replaced by pixels from the cat head image
+        # where pixels in the mask are marked as foreground (0xFF)
+        faceArea[catHeadMask == 255] = catHeadImg[catHeadMask == 255]
 
     # display the final image with detection borders
     cv2.imshow("Detected Heads", img)
